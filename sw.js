aeb63bbcb32e051c8af89f0e06d7721a8ffcb9e2
Alexandra <75269203+alexandralit@users.noreply.github.com>
2024-02-25 18:45:31 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.addEventListener('install', (e) => {
   e.waitUntil(
-    caches.open('fox-store').then((cache) => cache.addAll([
+    caches.open('v1').then((cache) => cache.addAll([
       '/apps/',
       '/apps/index.html',
       '/apps/index.js',
