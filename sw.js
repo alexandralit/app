f60607317bb25c0024c4b74075469044ba0db11a
Alexandra <75269203+alexandralit@users.noreply.github.com>
2024-03-31 17:18:53 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.addEventListener('install', (e) => {
   e.waitUntil(
-    caches.open('v7').then((cache) => cache.addAll([
+    caches.open('v8').then((cache) => cache.addAll([
       '/apps/',
       '/apps/index.html',
       '/apps/index.js',
